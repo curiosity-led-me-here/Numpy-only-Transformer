df7d771df5aa2da4f12831032a1e1719541f3fa9
morphed_human <163374239+curiosity-led-me-here@users.noreply.github.com>
2026-05-25 00:38:52 +0530

    plotted out the computation graph for backward pass, calculated the gradients of components like layernorm in my workings.

--- expt.py
+++ expt.py
@@ -1,10 +1,11 @@
 import numpy as np
+from main import GPT
 
-lst = np.random.randn(7,10)
-new_L = []
-L1 = lst[:, :5]
-L2 = lst[:, 5:]
-for i in [L1, L2]:
-    new_L.append(i)
-new_L = np.concatenate(new_L, axis=1)
-print(new_L.shape)
+np.set_printoptions(suppress=True, precision=8)
+X = np.random.randint(low=0, high=100, size=10)
+X_train = X[:-1]
+Y = X[1:]
+
+model = GPT(dim=64, head_size=4, vocab_size=101, X=X_train, Y=Y)
+prediction = model.full_forward()
+print(prediction)
--- main.py
+++ main.py
@@ -18,9 +18,9 @@ class GPT():
         # layernorm params
         self.MHA_alpha, self.MHA_beta, self.Ot_alpha, self.Ot_beta, self.mha_cross_alpha, self.mha_cross_beta, self.Of_alpha, self.Of_beta  = np.random.randn(8, 1, self.inner_dim)
         # X-head
-        self.WxQ, self.WxK, self.WxV = np.random.randn(3, self.head_size, self.inner_dim, self.inner_dim / self.head_size)
+        self.WxQ, self.WxK, self.WxV = np.random.randn(3, self.head_size, self.inner_dim, int(self.inner_dim / self.head_size))
         # Y-head
-        self.WyQ, self.WyK, self.WyV = np.random.randn(3, self.head_size, self.inner_dim, self.inner_dim / self.head_size)
+        self.WyQ, self.WyK, self.WyV = np.random.randn(3, self.head_size, self.inner_dim, int(self.inner_dim / self.head_size))
         
         '''
         Important concept: Initially I was slicing the input vector into heads and calculating separate heads with same static QKV weights. That is not the intent of attention. We need diversity.
@@ -66,8 +66,10 @@ class GPT():
          return np.concatenate(Zr, axis=1)
 
     def softmax(self, x):
-        exp = np.exp(x)
-        return exp / np.sum(exp, axis=-1)
+        x_max = np.max(x, axis=-1, keepdims=True) 
+        exp = np.exp(x - x_max) 
+    
+        return exp / np.sum(exp, axis=-1, keepdims=True)
 
     def layernorm(self, residue, alpha, beta):
         avg = np.average(residue, axis=1, keepdims=True)
@@ -94,7 +96,7 @@ class GPT():
         Ot = self.layernorm(residue=ffn_residue, alpha=Ot_alpha, beta=Ot_beta)
         return Ot
 
-    def forward_Y(self, Y_input, X_output, embed, inner_dim, WQ, WK, WV, WZ, MHA_alpha, MHA_beta, Wf1, Wf2, Bf1, Bf2, Of_alpha, Of_beta):
+    def forward_Y(self, Y_input, X_output, embed, inner_dim, WQ, WK, WV, WZ, MHA_alpha, MHA_beta):
         Y = embed[Y_input] / np.sqrt(inner_dim) # n x d
         Zy = self.positional_encode(Y)
         MHAy = self.attention(Zy, WQ, WK, WV) @ WZ
@@ -115,9 +117,8 @@ class GPT():
 
     def full_forward(self):
         X_output = self.forward_X(X_input=self.X, embed=self.embed, inner_dim=self.inner_dim, WQ=self.WxQ, WK=self.WxK, WV=self.WxV, WZ=self.WxZ, MHA_alpha=self.MHA_alpha, MHA_beta=self.MHA_beta, Wf1=self.WXf1, Wf2=self.WXf2, Bf1=self.BXf1, Bf2=self.BXf2, Ot_alpha=self.Ot_alpha, Ot_beta=self.Ot_beta)
-        Y_output = self.forward_Y(Y_input=self.Y, X_output=X_output, inner_dim=self.inner_dim, WQ=self.WyQ, WK=self.WyK, WV=self.WyV, WZ=self.WyZ, MHA_alpha=self.mha_cross_alpha, MHA_beta=self.mha_cross_beta)
+        Y_output = self.forward_Y(Y_input=self.Y, X_output=X_output, embed=self.embed, inner_dim=self.inner_dim, WQ=self.WyQ, WK=self.WyK, WV=self.WyV, WZ=self.WyZ, MHA_alpha=self.mha_cross_alpha, MHA_beta=self.mha_cross_beta)
         logits = Y_output @ self.W_out + self.B_out
         prediction = self.softmax(logits)
         return prediction
-        
-#masking and padding
+
